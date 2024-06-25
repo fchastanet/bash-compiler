3d8585eaeaf3823f3f921705e75c57f1c9e66561
François Chastanet <fchastanet@wiley.com>
2024-06-25 23:22:16 +0200

    clean coding : moved model merger into compiler

---
+++ internal/model/loadModel.go
@@ -0,0 +1,120 @@
+package model
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"slices"
+	"strings"
+
+	"github.com/goccy/go-yaml"
+)
+
+func WriteYamlFile(modelMap map[string]interface{}, tempYamlFile os.File) (err error) {
+	yamlResult, err := yaml.Marshal(modelMap)
+	if err != nil {
+		return err
+	}
+	_, err = tempYamlFile.Write(yamlResult)
+	if err != nil {
+		return err
+	}
+	log.Printf("Temp file containing resulting yaml file : %s\n", tempYamlFile.Name())
+	return nil
+}
+
+func LoadModel(referenceDir string, modelFilePath string, resultMap *map[string]interface{}) (err error) {
+	// read one yaml file
+	referenceDirs := []string{referenceDir}
+	model := map[string]interface{}{}
+	err = decodeFile(modelFilePath, referenceDirs, &model)
+	if err != nil {
+		return err
+	}
+
+	if _, ok := model["extends"]; ok {
+		extends := model["extends"].([]interface{})
+		for _, file := range extends {
+			fileAbs := filepath.Join(referenceDir, file.(string))
+			if _, err := os.Stat(fileAbs); err != nil {
+				return err
+			}
+			extendsMap := map[string]interface{}{}
+			err = decodeFile(fileAbs, referenceDirs, &extendsMap)
+			if err != nil {
+				return err
+			}
+			*resultMap = mergeMaps(resultMap, &extendsMap)
+		}
+	}
+
+	*resultMap = mergeMaps(resultMap, &model)
+	delete(*resultMap, "extends")
+
+	return nil
+}
+
+func decodeFile(
+	file string, referenceDirs []string, myMap *map[string]interface{},
+) error {
+	fileReader, err := os.Open(file)
+	if err != nil {
+		return err
+	}
+
+	dec := yaml.NewDecoder(fileReader, yaml.ReferenceDirs(referenceDirs...))
+	if err := dec.Decode(myMap); err != nil {
+		return err
+	}
+	return nil
+}
+
+func compareObjects(o1 interface{}, o2 interface{}) int {
+	v1, ok1 := o1.(string)
+	v2, ok2 := o2.(string)
+	if ok1 && ok2 {
+		return strings.Compare(v1, v2)
+	}
+	return 1
+}
+
+func mergeMaps(map1 *map[string]interface{}, map2 *map[string]interface{}) map[string]interface{} {
+	out := make(map[string]interface{}, len(*map1))
+	// copy map1 to out
+	for k, v := range *map1 {
+		out[k] = v
+	}
+	for k, map2v := range *map2 {
+		// if key does not exists in map1, just append
+		map1v, ok := out[k]
+		if !ok {
+			out[k] = map2v
+			continue
+		}
+		if v2, ok := map2v.(map[string]interface{}); ok {
+			// map2v is a map
+			if v1, ok := map1v.(map[string]interface{}); ok {
+				// if map1v is a map  too, we merge with map2v
+				out[k] = mergeMaps(&v1, &v2)
+				continue
+			}
+		} else if v2, ok := map2v.([]interface{}); ok {
+			// map2v is an array, concat
+			if out[k] == nil {
+				out[k] = []any{}
+			}
+			out1 := out[k].([]interface{})
+			arr := append([]any{}, out1...)
+			arr = append(arr, v2...)
+			// remove duplicates
+			slices.SortFunc(arr, compareObjects)
+			arr = slices.CompactFunc(arr, func(o1 interface{}, o2 interface{}) bool {
+				return compareObjects(o1, o2) == 0
+			})
+			out[k] = arr
+			continue
+		}
+		out[k] = map2v
+	}
+	return out
+}
