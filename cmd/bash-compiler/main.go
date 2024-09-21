eab8c4278d883451577455b4cff06695ac83adff
François Chastanet <fchastanet@wiley.com>
2024-09-21 10:15:09 +0200

    moved main instruction to CompilerPipelineService

--- cmd/bash-compiler/main.go
+++ cmd/bash-compiler/main.go
@@ -2,69 +2,12 @@
 package main
 
 import (
-	"log/slog"
 	"os"
-	"path/filepath"
 
-	"github.com/fchastanet/bash-compiler/internal/compiler"
-	"github.com/fchastanet/bash-compiler/internal/model"
-	"github.com/fchastanet/bash-compiler/internal/render"
 	"github.com/fchastanet/bash-compiler/internal/services"
-	"github.com/fchastanet/bash-compiler/internal/utils/dotenv"
-	"github.com/fchastanet/bash-compiler/internal/utils/files"
 	"github.com/fchastanet/bash-compiler/internal/utils/logger"
 	"go.uber.org/automaxprocs/maxprocs"
 )
-
-func skipIntermediateFilesCallback(
-	_ string, _ string, _ string, _ string,
-) error {
-	return nil
-}
-
-// load .bash-compiler file in current directory if exists
-func loadConfFile(cli *cli) {
-	configFile := filepath.Join(string(cli.RootDirectory), ".bash-compiler")
-	err := files.FileExists(configFile)
-	if err == nil {
-		slog.Info("Loading", logger.LogFieldFilePath, configFile)
-		err = dotenv.LoadEnvFile(configFile)
-		logger.Check(err)
-	} else {
-		slog.Warn("Config file is not available or not readable", "configFile", configFile)
-	}
-}
-
-func getYamlFiles(cli *cli) (yamlFiles YamlFiles, err error) {
-	yamlFiles = cli.YamlFiles
-	if len(cli.YamlFiles) == 0 {
-		filesList, err := files.MatchPatterns(
-			string(cli.RootDirectory),
-			"**/*"+string(cli.BinaryFilesExtension),
-		)
-		logger.Check(err)
-		if len(filesList) == 0 {
-			slog.Error(
-				"cannot find any file with specified suffix and directory",
-				"rootDirectory", string(cli.RootDirectory),
-				"extension", cli.BinaryFilesExtension,
-			)
-			return cli.YamlFiles, err
-		}
-		yamlFiles = filesList
-	}
-	return yamlFiles, nil
-}
-
-func setEnvVariable(name string, value string) {
-	slog.Debug(
-		"main",
-		logger.LogFieldVariableName, name,
-		logger.LogFieldVariableValue, value,
-	)
-	err := os.Setenv(name, value)
-	logger.Check(err)
-}
 
 func main() {
 	// This controls the maxprocs environment variable in container runtimes.
@@ -82,59 +25,18 @@ func main() {
 	var cli cli
 	err = parseArgs(&cli)
 	logger.Check(err)
-
-	// set useful env variables that can be interpolated during template rendering
-	setEnvVariable("COMPILER_ROOT_DIR", string(cli.CompilerRootDir))
-	setEnvVariable("ROOT_DIR", string(cli.RootDirectory))
-
-	// load config file
-	loadConfFile(&cli)
 	logger.InitLogger(cli.LogLevel)
-	if cli.Debug {
-		envVars := os.Environ()
-		for _, envVar := range envVars {
-			slog.Debug("env", "var", envVar)
-		}
-	}
 
-	// create BinaryModelService
-	templateContext := render.NewTemplateContext()
-	requireAnnotationProcessor := compiler.NewRequireAnnotationProcessor()
-	embedAnnotationProcessor := compiler.NewEmbedAnnotationProcessor()
-	compilerService := compiler.NewCompiler(
-		templateContext,
-		[]compiler.AnnotationProcessorInterface{
-			requireAnnotationProcessor,
-			embedAnnotationProcessor,
-		},
+	compilerPipelineService := services.NewCompilerPipelineService(
+		string(cli.RootDirectory),
+		string(cli.CompilerRootDir),
+		string(cli.TargetDir),
+		[]string(cli.YamlFiles),
+		string(cli.BinaryFilesExtension),
+		cli.Debug,
+		cli.KeepIntermediateFiles,
 	)
-	var templateContextInterface services.TemplateContextInterface = templateContext
-	var compilerInterface services.CodeCompilerInterface = compilerService
-	intermediateFileCallback := skipIntermediateFilesCallback
-	intermediateFileContentCallback := skipIntermediateFilesCallback
-	if cli.KeepIntermediateFiles {
-		intermediateFileCallback = logger.DebugCopyIntermediateFile
-		intermediateFileContentCallback = logger.DebugSaveIntermediateFile
-	}
-	binaryModelService := services.NewBinaryModelService(
-		model.NewBinaryModelLoader(),
-		templateContextInterface,
-		compilerInterface,
-		intermediateFileCallback,
-		intermediateFileContentCallback,
-	)
-	defaultLogger := slog.Default()
-
-	yamlFiles, err := getYamlFiles(&cli)
+	compilerPipelineService.Init()
+	err = compilerPipelineService.ProcessPipeline()
 	logger.Check(err)
-	for _, binaryModelFilePath := range yamlFiles {
-		slog.SetDefault(defaultLogger.With("binaryModelFilePath", binaryModelFilePath))
-		binaryModelServiceContextData, err := binaryModelService.Init(
-			string(cli.TargetDir),
-			binaryModelFilePath,
-		)
-		logger.Check(err)
-		err = binaryModelService.Compile(binaryModelServiceContextData)
-		logger.Check(err)
-	}
 }
